dee508c01985ea3ab71dbfcb7a3f4f6ffbb8ab8f
Abderrazak BOUADMA <abderrazak.bouadma@binance.com>
2022-12-18 23:10:39 +0100

    fix-vars

--- variables.tf
+++ variables.tf
@@ -1,16 +1,13 @@
 variable "region" {
   description = "AWS region"
-  default = "us-east-1"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default = "Provisioned by Terraform"
 }
 
 variable "AWS_ACCESS_KEY_ID" {}
