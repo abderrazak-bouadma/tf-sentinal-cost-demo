fc5b1d5e19f3bd97c3309d12466cc5fe1b73e6a3
Abderrazak BOUADMA <abderrazak.bouadma@binance.com>
2022-12-18 23:12:30 +0100

    adding-ec2-instance

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -25,11 +25,12 @@ resource "aws_subnet" "tf_demo_subnet" {
 }
 
 # Path: 3-ec2.tf
-# resource "aws_instance" "tf_demo_ec2" {
-#   ami = "ami-0c55b159cbfafe1f0"
-#   instance_type = "t2.micro"
-#   subnet_id = aws_subnet.tf_demo_subnet.id
-#   tags = {
-#     Name = "tf_demo_ec2"
-#   }
-# }
+resource "aws_instance" "tf_demo_ec2" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = var.instance_type
+  instance_name = var.instance_name
+  subnet_id = aws_subnet.tf_demo_subnet.id
+  tags = {
+    Name = "tf_demo_ec2"
+  }
+}
